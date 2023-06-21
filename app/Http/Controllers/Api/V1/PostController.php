e52227ca3f592b86df467f7bdba3792f202419b7
GabrielDLY <negriarroyo7@gmail.com>
2023-06-21 17:30:12 -0300

    final-de-la-V1

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Http\Resources\V1\PostResource;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return PostResource::collection(Post::latest()->paginate()) ;
+    }
+
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json([
+            'message' => 'Success'
+        ]);
+    }
+}
