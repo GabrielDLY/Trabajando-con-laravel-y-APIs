175723f74602ee2788db2bcd87e3cbba2d69ea0c
GabrielDLY <negriarroyo7@gmail.com>
2023-06-22 20:54:42 -0300

    Ultima modificacion

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)
-    ->only(['index', 'show', 'destroy']);
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+
+//Version 1
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
+
+
+//Version 2
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['index', 'show'])
+    ->middleware('auth:sanctum');    
+
+Route::post('login', [
+    App\Http\Controllers\Api\LoginController::class, 
+    'login'
+]);
